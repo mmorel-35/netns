277db89c8cb8d5425ab181224e5fa5092b84768e
Matthieu MOREL <matthieu.morel35@gmail.com>
2023-02-06 07:35:54 +0100

    Update doc.go

--- doc.go
+++ doc.go
@@ -6,5 +6,4 @@
 // accomplished via a `defer ns.Close()` on the handle. Changing namespaces
 // requires elevated privileges, so in most cases this code needs to be run
 // as root.
-
 package netns
